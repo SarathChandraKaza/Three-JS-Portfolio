5896ed197822cf40965d4204aac161153c4a4a35
SarathChandraInfiVR <148770658+SarathChandraInfiVR@users.noreply.github.com>
2024-12-04 09:51:00 +0530

    Revert "Changed repo name"
    This reverts commit c1c91574663461525b1deb819edee3dc38d01a89.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 export default {
     root: 'src/', // Source directory for the Vite project
     publicDir: '../static/', // Directory for public assets
-    base: '/XR-Portfolio/', // Replace with your repository name
+    base: '/Three-JS-Portfolio/', // Replace with your repository name
     build: {
       outDir: '../docs', // Output directory for build files
       emptyOutDir: true, // Clears the output directory before building
