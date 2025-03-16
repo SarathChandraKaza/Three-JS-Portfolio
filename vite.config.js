071c98a0329a133543e53e54b289cb28ef3b0475
SarathChandra <workmail.sarath@gmail.com>
2025-03-16 20:41:37 +0530

    Test build 5

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: './',
+  base: '/Three-JS-Portfolio/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
